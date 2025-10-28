746600e2cdf40d8da6f76685d66dc9c7e07909dd
Pableis05 <guzmanpablo035@gmail.com>
2025-10-28 17:45:32 -0500

    fix: validate realeaseDate

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,26 +1,26 @@
-import { IsArray, IsIn, IsOptional, IsString } from "class-validator";
+import { IsArray, IsDateString, IsIn, IsOptional, IsString } from "class-validator";
 import { Review } from "src/reviews/entities/review.entity";
 
 export class CreateMovieDto {
 
-    @IsString()
-    title: string;
-    
-    @IsString()
-    description: string;
-    
-    @IsString()
-    director: string;
-    
-    @IsString()
-    releaseDate: Date;
-    
-    @IsString()
-    @IsIn(['action', 'comedy', 'drama', 'horror', 'romance', 'sci-fi', 'thriller']) // hay que especificar
-    genre: string;
+  @IsString()
+  title: string;
+  
+  @IsString()
+  description: string;
+  
+  @IsString()
+  director: string;
+  
+  @IsDateString()
+  releaseDate: string;
+  
+  @IsString()
+  @IsIn(['action', 'comedy', 'drama', 'horror', 'romance', 'sci-fi', 'thriller'])
+  genre: string;
 
-    @IsArray()
-    @IsOptional()
-    reviews?: Review[];
-    
+  @IsArray()
+  @IsOptional()
+  reviews?: Review[];
+  
 }
